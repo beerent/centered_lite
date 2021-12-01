84a725495903aa457bf20c862066de212a5ced36
beerent <brentryczak@gmail.com>
2021-12-01 05:57:58 -0500

    converted App to component class

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 import './App.css';
 import GoogleSignIn from './GoogleSignIn';
 
@@ -13,15 +12,12 @@ function loginFailure(response: any) {
   console.log(response);
 }
 
-function App() {
-  // <GoogleSignIn loginSuccessCallback={loginSuccess} loginFailureCallback={loginFailure}
-  // <GoogleSignIn loginSuccessCallback={(response: any) => loginSuccess(response)} loginFailureCallback={(response: any) => loginFailure(response)} />
-  
-  return (
-    <div className="App">
-      <GoogleSignIn loginSuccessCallback={(response: any) => loginSuccess(response)} loginFailureCallback={(response: any) => loginFailure(response)} />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <GoogleSignIn loginSuccessCallback={(response: any) => loginSuccess(response)} loginFailureCallback={(response: any) => loginFailure(response)} />
+      </div>
+    );
+  }
 }
-
-export default App;
